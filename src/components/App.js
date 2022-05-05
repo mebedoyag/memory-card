9c5407a46ab3da562346b5b7ae5aa7acc37c192f
Miguel Bedoya <me.bedoya10@gmail.com>
2022-05-05 08:15:47 -0500

    do game logic of the new version

--- src/components/App.js
+++ src/components/App.js
@@ -13,84 +13,37 @@ function App() {
   const [bestScore, setBestScore] = useState(0);
   const [score, setScore] = useState(0);
 
-  
+  const resetStatus = () => {
+    animals.forEach(animal => {
+      animal.selected = false;
+    })
+  }
 
+  const handleRepetition = () => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+    setScore(0);
+  }
 
-  // const reordered = obj => {
-  //   return genRandArray(6).reduce((acc, current) => {
-  //     const temp = `animal${current}`;
-  //     acc[temp] = { ...sampleAnimals[temp] };
-
-  //     return acc;
-  //   }, {});
-  // }
-
-  // const loadSampleAnimals = () => {
-  //   // const reorderedAnimals = reordered(sampleAnimals);
-  //   setAnimals(sampleAnimals);
-  // }
-
-  // const shuffleAnimals = () => {
-  //   const reorderedAnimals = reordered(animals);
-  //   setAnimals(reorderedAnimals);
-  // }
-
-  // useEffect(() => {
-  //   if (score !== 0) {
-  //     const changeSelectedStatus = (cat) => {
-  //       const copyAnimal = {...animals[cat], selected: true };
-  //       const copyAnimals = { ...animals, [cat]: copyAnimal };
-  //       setAnimals(copyAnimals);
-  //     }
-  //     changeSelectedStatus(currentAnimal);
-  //   }
-  // }, [score]);
-
-
-  // useEffect(() => {
-  //   const resetSelectedStatus = () => {
-  //     return Object.keys(animals).reduce((acc, current) => {
-  //       const copyAnimal = {...animals[current], selected: false };
-  //       acc[current] = copyAnimal;
-  //       return acc;
-  //     }, {})
-  //   }
-  //   setAnimals(resetSelectedStatus());
-  // }, [score]);
-
-
-  // const handleScore = (clickedCat) => {
-  //   if (animals[clickedCat].selected) {
-  //     console.log("i am here");
-  //     if (score > bestScore) {
-  //       setBestScore(score);
-  //     }
-  //     setScore(0);
-  //   } else {
-  //     setScore(score + 1);
-  //     setCurrentAnimal(clickedCat);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   shuffleAnimals();
-  // }, [score, bestScore]);
-
-  
-  
+  const handleIncrement = () => {
+    setScore(score + 1);
+  }
+    
   const handleScore = (id) => {
     animals.forEach(animal => {
       if (animal.id === id && animal.selected === false) {
-        console.log("YOU click a cat for first time");
         animal.selected = true;
+        handleIncrement();
+
       } else if (animal.id === id && animal.selected === true) {
-        console.log("YOU click a cat for second time");
+        handleRepetition();
+        resetStatus();
       }
     })
   }
   
   const shuffleAnimals = () => {
-    // console.log("YOU click the image");
     const animalsCopy = shuffleArray(animals);
     setAnimals(animalsCopy);
   }
@@ -101,7 +54,6 @@ function App() {
   }
   
   const loadSampleAnimals = () => {
-    // console.log("YOU want to loadSampleAnimals");
     setAnimals(sampleAnimals);
   }
   
